a6b5ac11bf0d21980a4076324e80754fd3e29e90
Kenn Kasparek <kasparek30@yahoo.com>
2026-06-03 14:27:05 -0500

    next.config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@sparticuz/chromium/**/*",
+      "./node_modules/puppeteer-core/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
